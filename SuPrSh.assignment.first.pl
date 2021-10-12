7928e2ce8b69563a527357ceb2dd038cbc5875f3
Surya P <45768049+SuPrSh@users.noreply.github.com>
2021-10-12 21:20:57 +0530

    SuPrSh.assignment.first

---
+++ SuPrSh.assignment.first.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+$name="Surya";
+$email="suryapdsharma\@gmail.com";
+$discord="SuPrSh";
+$biostack="Genomics";
+$language="perl";
+
+print "My name is $name and below is my information \n";
+print "* Email: $email \n";
+print "* Discord: $discord \n";
+print "I am using $language to built this script \s";
+print "and my field of interest is $biostack";
