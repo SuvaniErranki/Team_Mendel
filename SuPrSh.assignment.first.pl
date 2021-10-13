0d17f5063ce7f2ea7a4a1a17ab2e7d90cc52b0b9
Surya P <45768049+SuPrSh@users.noreply.github.com>
2021-10-13 18:19:33 +0530

    Update SuPrSh.assignment.first.pl

--- SuPrSh.assignment.first.pl
+++ SuPrSh.assignment.first.pl
@@ -3,11 +3,7 @@
 $name="Surya";
 $email="suryapdsharma\@gmail.com";
 $discord="SuPrSh";
-$biostack="Genomics";
+$slack="Genomics";
 $language="perl";
 
-print "Name: $name\n";
-print "Email id: $email \n";
-print "Slack id: $discord \n";
-print "Language: $language \n";
-print "Biostack: $biostack";
+print "Name:$name, email-id:$email, slack-id:$slack, language:$language, biostack:$biostack";
