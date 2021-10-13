b9d54f7606ec454a8399334046c066d7299e7998
Surya P <45768049+SuPrSh@users.noreply.github.com>
2021-10-13 19:09:46 +0530

    Delete SuPrSh.assignment.first.pl

--- SuPrSh.assignment.first.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-$name="Surya";
-$email="suryapdsharma\@gmail.com";
-$slack="SuPrSh";
-$biostack="Genomics";
-$language="perl";
-
-print "Name:$name, email-id:$email, slack-id:$slack, language:$language, biostack:$biostack";
